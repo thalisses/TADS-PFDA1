e6c742183f6d62263feb457288b1a8d6988af8c6
thalissaj <thalissajenniffer@hotmail.com>
2020-09-15 21:20:49 -0300

    moving aula4 to solucoes

---
+++ solucoes/aula4/aula4ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    printf("Quadrados: (15 a 200)\n");
+
+    for (i = 15; i <= 200; i++)
+    {
+        printf("%d\n", i * i);
+    }
+    return 0;
+}
